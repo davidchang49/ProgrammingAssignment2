b1a19c7fabb7e5cadf8059e0e5db6eaf70dfac36
David Chang <davidchang49@yahoo.com>
2017-06-17 21:52:57 -0600

    Adding comments to functions

--- cachematrix.R
+++ cachematrix.R
@@ -1,24 +1,27 @@
-## Put comments here that give an overall description of what your
-## functions do
+## These functions help reduce processing time by make calculus available for reuse
 
-## Write a short comment describing this function
-
+##This function cache a Matrix into memory
 makeCacheMatrix <- function(x = matrix()) {
-		m <- NULL
+		m <- NULL  #initializin values
+		
+		#subfunction that sets matrix
         set <- function(y) {
                 x <<- y
                 m <<- NULL
         }
+		#retrieve matrix values
         get <- function() x
+		#sub function that sets inverse matrix value
         setinverse <- function(inverse) m <<- inverse
-        getinverse <- function() m
+        #sub function that gets inverse matrix value
+		getinverse <- function() m
         list(set = set, get = get,
              setinverse = setinverse,
              getinverse = getinverse)
 }
 
 
-## Write a short comment describing this function
+## This function calculate the inverse of a matrix and cache result into memory for later use
 
 cacheSolve <- function(x, ...) {
         ## Return a matrix that is the inverse of 'x'
@@ -29,6 +32,7 @@ cacheSolve <- function(x, ...) {
                 return(m)
         }
         data <- x$get()
+		#Applying r function that makes the inverse of a matrix
         m <- solve(data, ...)
         x$setinverse(m)
         m
